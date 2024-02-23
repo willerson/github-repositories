771e14af534586fbb96c1f6d38749a5b2750e1f1
Willerson Miranda <willerson.miranda@gmail.com>
2024-02-23 15:16:53 -0300

    :lipstick: feat: developed the component card

--- app/components/Card/card.tsx
+++ app/components/Card/card.tsx
@@ -1,23 +1,75 @@
+'use client';
 import React from 'react';
+import { HeartFilled } from '../icons/heart-filled';
+import { Heart } from '../icons/heart';
+import Favorite from './favorite';
+import { useState } from 'react';
+import { tv } from 'tailwind-variants';
 
-const Card = () => {
+const styles = tv({
+  slots: {
+    card: 'border border-border-line rounded p-4 flex space-x-2',
+    wrapperText: 'mb-4',
+    header: 'text-lg text-grey-neutral font-semibold mb-1.5',
+    paragraph: 'text-sm text-placeholder',
+    footer: 'w-64 flex justify-between items-center',
+    technologyType: 'flex items-center text-xs text-grey-neutral space-x-2',
+    technologyDot: 'inline-block h-4 w-4 rounded-full',
+    dateElement: 'text-xs text-grey-neutral',
+  },
+});
+
+const {
+  card,
+  wrapperText,
+  header,
+  paragraph,
+  footer,
+  technologyType,
+  technologyDot,
+  dateElement,
+} = styles();
+
+interface CardProps {
+  title?: string;
+  description?: string;
+  technology?: string;
+  date?: string;
+  dotColor?: string;
+}
+
+const Card = ({
+  title,
+  description,
+  technology,
+  date,
+  dotColor,
+}: CardProps) => {
+  const [favorite, setFavorite] = useState(false);
+
+  const handleFavorite = () => {
+    setFavorite(!favorite);
+  };
+
   return (
-    <div>
+    <div className={`${card()}`}>
       <div>
-        <div>
-          <h2>Pokepicker</h2>
-          <p>
-            Aplicativo de visualização de Pokémon utilizando o PokeAPI.co -
-            Construído em React Native (Expo)
+        <header className={`${wrapperText()}`}>
+          <h2 className={`${header()}`}>{title}</h2>
+          <p className={`${paragraph()}`}>{description}</p>
+        </header>
+        <footer className={`${footer()}`}>
+          <p className={`${technologyType()}`}>
+            <span className={`${technologyDot()} ${dotColor}`}></span>
+            <span>{technology}</span>
           </p>
-        </div>
-        <div>
-          <span>Typescript</span>
-          <span>Updated on 17 Apr 2021</span>
-        </div>
+          <span className={`${dateElement()}`}>{date}</span>
+        </footer>
       </div>
       <div>
-        <button></button>
+        <Favorite onClick={handleFavorite} state={favorite}>
+          {favorite ? <Heart /> : <HeartFilled />}
+        </Favorite>
       </div>
     </div>
   );
---
+++ app/components/Card/favorite.tsx
@@ -0,0 +1,27 @@
+import { VariantProps, tv } from 'tailwind-variants';
+
+const styles = tv({
+  base: 'border rounded-full w-10 h-10 flex items-center justify-center transition-colors',
+  variants: {
+    state: {
+      false: 'border-primary',
+      true: 'bg-white-matte',
+    },
+  },
+});
+
+interface FavoriteProps extends VariantProps<typeof styles> {
+  className?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+const Favorite = ({ className, onClick, children, state }: FavoriteProps) => {
+  return (
+    <button onClick={onClick} className={styles({ class: className, state })}>
+      {children}
+    </button>
+  );
+};
+
+export default Favorite;
