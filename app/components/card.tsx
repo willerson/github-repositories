6114a4224afeea511912d59eba243c5b6e8eda00
Willerson Miranda <willerson.miranda@gmail.com>
2024-02-23 09:34:20 -0300

    :tada: init: settings and initial commit

---
+++ app/components/card.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Pokepicker</h2>
+          <p>
+            Aplicativo de visualização de Pokémon utilizando o PokeAPI.co -
+            Construído em React Native (Expo)
+          </p>
+        </div>
+        <div>
+          <span>Typescript</span>
+          <span>Updated on 17 Apr 2021</span>
+        </div>
+      </div>
+      <div>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ app/services/api.tsx
@@ -0,0 +1,3 @@
+export const API_URL = 'https://api.github.com/users/';
+
+const token = process.env.GITHUB_TOKEN;
