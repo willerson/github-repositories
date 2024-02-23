b79fc09638a9af50ec37a42ab2498516b91bd696
Willerson Miranda <willerson.miranda@gmail.com>
2024-02-23 16:03:50 -0300

    :lipstick: feat: developed the component search

---
+++ app/components/Card/Search/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SearchIcon } from '../../icons/search';
+import { tv } from 'tailwind-variants';
+
+const styles = tv({
+  slots: {
+    wrapper:
+      'border border-border-line rounded py-2 px-4 flex justify-between items-center',
+    input: 'text-sm text-placeholder focus:outline-none',
+  },
+});
+
+const { wrapper, input } = styles();
+
+interface SearchProps {
+  onClick?: () => void;
+}
+
+const Search = ({ onClick }: SearchProps) => {
+  return (
+    <div className={`${wrapper()}`}>
+      <input
+        className={`${input()}`}
+        type="text"
+        placeholder="Buscar usuário"
+      />
+      <button onClick={onClick}>
+        <SearchIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import Card from './components/Card/card';
+import Search from './components/Card/Search';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card />
+      <Search />
       <Card />
     </main>
   );
